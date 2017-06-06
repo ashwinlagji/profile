5c2d46d0601ddcc04dd3ee6bb8ce974b84e4d728
ashwinlagji <ashwinlagji@gmail.com>
2017-06-06 22:22:27 +0530

    profile first draft complete

--- conf/webpack-dist.conf.js
+++ conf/webpack-dist.conf.js
@@ -23,7 +23,7 @@ module.exports = {
       },
 
       {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        test: /\.(png|woff|woff2|eot|ttf|svg|jpg|jpeg)$/,
         loader: 'url-loader?limit=100000'
       },
       {
